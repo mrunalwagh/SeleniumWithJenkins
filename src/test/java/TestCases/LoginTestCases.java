137d1c97317eda8e2047cbfcbdc42a335b869c69
mrunalwagh <mrunalwagh99@gmail.com>
2023-12-28 20:57:38 +0530

    my git test

---
+++ src/test/java/TestCases/LoginTestCases.java
@@ -0,0 +1,24 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class LoginTestCases extends BaseTest {
+
+
+    @Test
+    public void TEST_CASE_ONE() 
+    {
+        driver.get(URL);
+        loginPage.pressExc();
+    }
+
+    @Test
+    public void TEST_CASE_TWO() throws Exception {
+        driver.get(URL);
+       // loginPage.pressExc();
+       // homePage.searchForProducts();
+
+    }
+
+
+}
